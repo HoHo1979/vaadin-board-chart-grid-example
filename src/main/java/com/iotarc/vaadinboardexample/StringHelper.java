d30cf22da3e2a022be7079f9f814ecbe845ea96e
James Ho <jamesho19@gmail.com>
2017-11-17 09:22:29 +0800

    initial commit to test the Vaadin Board, Chart, and Grid to display temperature.

---
+++ src/main/java/com/iotarc/vaadinboardexample/StringHelper.java
@@ -0,0 +1,10 @@
+package com.iotarc.vaadinboardexample;
+
+public class StringHelper {
+
+	public static final String TIME="Time";
+	public static final String TEMP="Temperature";
+	public static final String CURRENT_TEMPERATURE = "Curent Temperature";
+	
+	
+}
---
+++ src/main/java/com/iotarc/vaadinboardexample/Temperature.java
@@ -0,0 +1,54 @@
+package com.iotarc.vaadinboardexample;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Temperature {
+	
+	
+	public static final String TIME="time";
+	public static final String TEMP="temp";
+	public static final String HEADER_TIME=StringHelper.TIME;
+	public static final String HEADER_TEMP=StringHelper.TEMP;
+	
+	
+	String key;
+	long time;
+	double temp;
+	
+	public Temperature(Long time, Double temp) {
+		this.time=time;
+		this.temp=temp;
+	}
+
+	public String getKey() {
+		return key;
+	}
+	
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
+	public long getTime() {
+		return time;
+	}
+	
+	public String getDate() {
+		return new SimpleDateFormat("yyyy/MM/dd hh:mm").format(new Date(time));
+	}
+	
+	public void setTime(long time) {
+		this.time = time;
+	}
+	
+	public double getTemp() {
+		return temp;
+	}
+	
+	public void setTemp(double temp) {
+		this.temp = temp;
+	}
+	
+	
+	
+}
